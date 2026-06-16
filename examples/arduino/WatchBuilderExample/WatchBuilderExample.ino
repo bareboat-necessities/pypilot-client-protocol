e8282e1d169a7f8d34729407baf133b40b2faa09
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-06-15 22:01:33 -0400

    Use AVR-safe protocol header in watch builder example

--- examples/arduino/WatchBuilderExample/WatchBuilderExample.ino
+++ examples/arduino/WatchBuilderExample/WatchBuilderExample.ino
@@ -1,12 +1,20 @@
 #include <Arduino.h>
-#include <pypilot_client_protocol.hpp>
+#include <pypilot_client_protocol_arduino.hpp>
 
 void setup() {
   Serial.begin(115200);
   while (!Serial) {}
-  Serial.print(pypilot_client_protocol::make_watch_periodic("ap.heading", 0.5).c_str());
-  Serial.print(pypilot_client_protocol::make_set_string("ap.mode", "compass").c_str());
-  Serial.print(pypilot_client_protocol::make_set_number("ap.heading_command", 90.0).c_str());
+
+  char line[128];
+
+  pypilot_client_protocol::make_watch_periodic(line, sizeof(line), "ap.heading", 0.5);
+  Serial.print(line);
+
+  pypilot_client_protocol::make_set_string(line, sizeof(line), "ap.mode", "compass");
+  Serial.print(line);
+
+  pypilot_client_protocol::make_set_number(line, sizeof(line), "ap.heading_command", 90.0);
+  Serial.print(line);
 }
 
 void loop() {}
